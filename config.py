d1f5cd607d8f9dc012f5c7b7ed90f6682c92b8e2
Almortagel <105941198+Almortage@users.noreply.github.com>
2023-04-08 17:56:11 +0200

    Update config.py

--- config.py
+++ config.py
@@ -17,16 +17,16 @@ LOG_GROUP_ID = int(getenv("LOG_GROUP_ID"))
 MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "cr")
 
 OWNER_ID = list(
-  map(int, getenv("OWNER_ID", "").split())) + [5881570606]
+  map(int, getenv("OWNER_ID", "").split())) + [5089553588]
 HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
 
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/Tompriv/Tom")
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/Almortage/sumusice")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/PP_G3")
-SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/JK_ZG")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/AlmortagelTech")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/AlmortagelTech2")
 
 SUPPORT_HEHE = SUPPORT_GROUP.split("me/")[1]
 
@@ -76,7 +76,7 @@ clean = {}
 autoclean = []
 
 
-START_IMG_URL = getenv("START_IMG_URL", "https://telegra.ph/file/e7bb54b34faadd2c9b199.jpg")
+START_IMG_URL = getenv("START_IMG_URL", "https://telegra.ph/file/14c7948ad180050fe16e4.jpg")
 
 PING_IMG_URL = getenv(
     "PING_IMG_URL",
@@ -135,4 +135,4 @@ if PING_IMG_URL:
 if START_IMG_URL:
     if START_IMG_URL != "assets/Ping.jpeg":
         if not re.match("(?:http|https)://", START_IMG_URL):
-            START_IMG_URL = "https://telegra.ph/file/e7bb54b34faadd2c9b199.jpg"
+            START_IMG_URL = "https://telegra.ph/file/14c7948ad180050fe16e4.jpg"
