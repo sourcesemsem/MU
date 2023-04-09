b8d35e9dc1198b028e2a49957e4f32ccf704cc47
Almortagel <105941198+Almortage@users.noreply.github.com>
2023-04-09 11:12:43 +0200

    Update settings.py

--- AnonX/utils/inline/settings.py
+++ AnonX/utils/inline/settings.py
@@ -24,7 +24,7 @@ def setting_markup(_):
         ],
         [
             InlineKeyboardButton(
-                text="⌞ 𝘾𝙍 • 𝙎𝙊𝙐𝙍𝘾𝙀 ⌝", url=f"https://t.me/pp_g3",
+                text="⌞ѕᴏụʀᴄᴇ ᴀʟᴍᴏʀᴛᴀɢᴇʟ⌝", url=f"https://t.me/AlmortagelTech",
             ),
             InlineKeyboardButton(
                 text=_["CLOSE_BUTTON"], callback_data="close"
