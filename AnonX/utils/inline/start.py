69af26752c1774bdf1a3a796d779ba01f28fbc9b
سـورس سـيـمـو <122203365+sourcesemsem@users.noreply.github.com>
2023-04-14 15:43:35 +0200

    Update start.py

--- AnonX/utils/inline/start.py
+++ AnonX/utils/inline/start.py
@@ -9,13 +9,13 @@ def start_pannel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
     buttons = [
         [
             InlineKeyboardButton(
-                text="اضغط لاضافتي لمجموعتك ✅",
-                url=f"https://t.me/{BOT_USERNAME}?startgroup=true"
+                text="اضغط لاضافتي لمجموعتك✅",
+                url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
             )
         ],
         [
             InlineKeyboardButton(
-                text="🌐- الاوامر -🌐",
+                text="🗒 الاوامر",
                 callback_data="settings_back_helper",
             ),
             InlineKeyboardButton(
