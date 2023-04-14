ed74d0f82253a4a494b883c0b5ae663ad838e664
سـورس سـيـمـو <122203365+sourcesemsem@users.noreply.github.com>
2023-04-14 07:18:24 +0200

    Update xgame.py

--- AnonX/plugins/xgame.py
+++ AnonX/plugins/xgame.py
@@ -8,13 +8,13 @@ from AnonX import app
 import re
 import sys
 
-GAME_MESSAGE = "⩹━★⊷━⌞ѕᴏụʀᴄᴇ ᴀʟᴍᴏʀᴛᴀɢᴇʟ⌝━⊶★━⩺\n\n★¦ مرحبا بك عزيزي:\n★¦في قسم العاب المرتجل\n\n⩹━★⊷━⌞ѕᴏụʀᴄᴇ ᴀʟᴍᴏʀᴛᴀɢᴇʟ⌝━⊶★━⩺"
+GAME_MESSAGE = "★¦ مرحبا بك عزيزي:\n★¦في قسم العاب سيمو\n\n"
 GAME_BUTTONS = [
     [ 
         InlineKeyboardButton ('★¦العاب 3D', callback_data= 'GAME1'),
-        InlineKeyboardButton ('ᴀʟᴍᴏʀᴛᴀɢᴇʟ', callback_data= 'GAME2'),
+        InlineKeyboardButton ('‹ 𝚜 𝚎 𝚖 𝚘 ›⁩', callback_data= 'GAME2'),
         ],[
-        InlineKeyboardButton ('⌞ѕᴏụʀᴄᴇ ᴀʟᴍᴏʀᴛᴀɢᴇʟ⌝⚡️', url =f"https://t.me/AlmortagelTech")              
+        InlineKeyboardButton ('‹ 𝚜𝚘𝚞𝚛𝚌𝚎 𝚜𝚎𝚖𝚘 ›⁩', url =f"https://t.me/FTTUTY")              
                  ],[
                 InlineKeyboardButton(
                         "◁", callback_data="close"),
@@ -308,7 +308,7 @@ async def kaompj(client, message):
 )
 async def zohary(client: Client, message: Message):
     await message.reply_photo(
-        photo=f"https://telegra.ph/file/14c7948ad180050fe16e4.jpg",
+        photo=f"https://telegra.ph/file/2e7e7b9b463149c1d00cb.jpg",
         caption= GAME_MESSAGE,
         reply_markup=InlineKeyboardMarkup(GAME_BUTTONS)
     )  
@@ -316,7 +316,7 @@ async def zohary(client: Client, message: Message):
 async def callback_query(client, CallbackQuery):
           if CallbackQuery.data == "GAME1":
             
-             GAME1_MESSAGE = "⩹━★⊷━⌞ѕᴏụʀᴄᴇ ᴀʟᴍᴏʀᴛᴀɢᴇʟ⌝━⊶★━⩺\n\nمرحبا بك في قسم العاب المرتجل 3D\n\n⩹━★⊷━⌞ѕᴏụʀᴄᴇ ᴀʟᴍᴏʀᴛᴀɢᴇʟ⌝━⊶★━⩺"
+             GAME1_MESSAGE = "مرحبا بك في قسم الالعاب"
 
              GAME1_BUTTONS = [
                  [
@@ -360,14 +360,14 @@ async def callback_query(client, CallbackQuery):
               )
           elif CallbackQuery.data == "GAME":
                
-               RETURN_GAME = "⩹━★⊷━⌞ѕᴏụʀᴄᴇ ᴀʟᴍᴏʀᴛᴀɢᴇʟ⌝━⊶★━⩺\n\n★¦مرحبا بك في قسم العاب المرتجل\n★¦اختار ما تشاء من الالعاب مسليه وستمتع بها\n\n⩹━★⊷━⌞ѕᴏụʀᴄᴇ ᴀʟᴍᴏʀᴛᴀɢᴇʟ⌝━⊶★━⩺" 
+               RETURN_GAME = "★¦مرحبا بك في قسم العاب سيمو\n★¦اختار ما تشاء من الالعاب مسليه وستمتع بها\n\n" 
 
                RETURN_BUTTON = [
                     [ 
                       InlineKeyboardButton ('★¦العاب 3D', callback_data= 'GAME1'),
-                      InlineKeyboardButton ('★¦العاب المرتجل', callback_data= 'GAME2')
+                      InlineKeyboardButton ('★¦العاب سيمو', callback_data= 'GAME2')
                       ],[
-        InlineKeyboardButton ('⌞ѕᴏụʀᴄᴇ ᴀʟᴍᴏʀᴛᴀɢᴇʟ⌝⚡️', url =f"https://t.me/AlmortagelTech")              
+        InlineKeyboardButton ('‹ 𝚜𝚘𝚞𝚛𝚌𝚎 𝚜𝚎𝚖𝚘 ›⁩', url =f"https://t.me/FTTUTY")              
                  ],[
                 InlineKeyboardButton(
                         "◁", callback_data="close"),
@@ -380,7 +380,7 @@ async def callback_query(client, CallbackQuery):
                     )
           elif CallbackQuery.data == "GAME2":
                
-               SOURCE_GAME = "⩹━★⊷━⌞ѕᴏụʀᴄᴇ ᴀʟᴍᴏʀᴛᴀɢᴇʟ⌝━⊶★━⩺\n\n★¦العاب المرتجل\n★¦كت\n★¦تويت\n★¦اسال\n★¦اصراحه\n\n⩹━★⊷━⌞ѕᴏụʀᴄᴇ ᴀʟᴍᴏʀᴛᴀɢᴇʟ⌝━⊶★━⩺." 
+               SOURCE_GAME = "★¦العاب سيمو\n★¦كت\n★¦تويت\n★¦اسال\n★¦اصراحه\n\n" 
 
                SORGAM_BUTTON = [
                     [ 
