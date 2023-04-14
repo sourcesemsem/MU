320125328a10e89d0cd45adb90edbfb4b7ac7ff0
سـورس سـيـمـو <122203365+sourcesemsem@users.noreply.github.com>
2023-04-14 07:05:25 +0200

    Update start.py

--- AnonX/utils/inline/start.py
+++ AnonX/utils/inline/start.py
@@ -15,7 +15,7 @@ def start_pannel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
         ],
         [
             InlineKeyboardButton(
-                text="الاوامر 📚",
+                text="🌐- الاوامر -🌐",
                 callback_data="settings_back_helper",
             ),
             InlineKeyboardButton(
@@ -36,7 +36,7 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
         ],
         [
             InlineKeyboardButton(
-                text="الاوامر 📚", callback_data="settings_back_helper"
+                text="🌐- الاوامر -🌐", callback_data="settings_back_helper"
             )
         ],
         [
