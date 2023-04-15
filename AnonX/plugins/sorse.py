d6fe93c9a40952cec1418b4f9bd6372586d28520
سـورس سـيـمـو <122203365+sourcesemsem@users.noreply.github.com>
2023-04-15 06:21:35 +0200

    Update sorse.py

--- AnonX/plugins/sorse.py
+++ AnonX/plugins/sorse.py
@@ -68,3 +68,19 @@ async def ihd(client: Client, message: Message):
 
 
 
+
+
+@app.on_message(command(["غنيلي", "غني", "صوره", "🎙 ¦ غـنيـلي"]))
+async def ihd(client: Client, message: Message):
+    rs = random.randint(39,65)
+    url = f"https://t.me/GTTUTY/{rs}"
+    await client.send_photo(message.chat.id,url,caption="🔥 ¦ تـم اختيـار الصوره لـك",parse_mode="html",
+    reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        message.from_user.first_name, url=f"https://t.me/{message.from_user.username}")
+                ],
+            ]
+        )
+    )
